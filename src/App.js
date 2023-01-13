cc48461d5befccf4533c110dfb72164e5c2e4d7c
Uday <uday.kumar@block8.com>
2023-01-13 10:43:13 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello React</p>
+        <p>Hello Docker</p>
       </header>
     </div>
   );
